f296a507c4baa5191cd2d3fa07ffa4e1827709c7
Matthew Atterholt <29619087+matterholt@users.noreply.github.com>
2021-10-28 23:47:15 -0400

    update holiday list

--- src/utils/HolidayDates.js
+++ src/utils/HolidayDates.js
@@ -1,70 +1,70 @@
 const DATA = {
-  holiday: [
+  nationalHolidays: [
     {
       id: 100,
       eventName: "New Years Day",
-      date: "2021-01-01",
+      dateStart: "2021-01-01",
       isActive: true,
       type: "holiday"
     },
     {
       id: 101,
       eventName: "Good Friday",
-      date: "2021-04-02",
+      dateStart: "2021-04-02",
       isActive: true,
       type: "holiday"
     },
     {
       id: 102,
       eventName: "Memorial Day",
-      date: "2021-05-31",
+      dateStart: "2021-05-31",
       isActive: true,
       type: "holiday"
     },
     {
       id: 103,
       eventName: "Independence Day",
-      date: "2021-07-01",
+      dateStart: "2021-07-01",
       isActive: true,
       type: "holiday"
     },
     {
       id: 104,
       eventName: "Labor Day",
-      date: "2021-09-06",
+      dateStart: "2021-09-06",
       isActive: true,
       type: "holiday"
     },
     {
       id: 105,
       eventName: "Thanksgiving Day",
-      date: "2021-11-25",
+      dateStart: "2021-11-25",
       isActive: true,
       type: "holiday"
     },
     {
       id: 106,
       eventName: "Thanksgiving Holiday",
-      date: "2021-11-25",
+      dateStart: "2021-11-25",
       isActive: true,
       type: "holiday"
     },
     {
       id: 107,
       eventName: "Chirstmas Holiday",
-      date: "2021-12-24",
+      dateStart: "2021-12-24",
       isActive: true,
       type: "holiday"
     },
     {
       id: 108,
       eventName: "New Years Eve",
-      date: "2021-13-31",
+      dateStart: "2021-13-31",
       isActive: true,
       type: "holiday"
     }
   ],
-  shutdown: [
+  sceduledHolidays: [
     {
       id: 200,
       eventName: "Summer Shutdown",
